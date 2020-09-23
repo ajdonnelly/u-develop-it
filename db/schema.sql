f9677805745ef0d3baf475c6bb19a1a3b0b74d9d
Alex Donnelly <a.james.donnelly@gmail.com>
2020-09-22 20:52:49 -0500

    add join query to SQL statements in gets

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,15 @@
+-- This will drop/delete the tables every time you run the migration script, thus ensuring you're starting with a clean slate.--
+DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS candidates;
+
 CREATE TABLE candidates (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  industry_connected BOOLEAN NOT NULL,
+  party_id INTEGER UNSIGNED,
+  --constraint. This allows us to flag the party_id field as an official foreign key and tells SQL which table and field it references. In this case, it references the id field in the parties table. This ensures that no id can be inserted into the candidates table if it doesn't also exist in the parties table. --
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
 
 CREATE TABLE parties (
